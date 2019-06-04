a967634bab9e733a63c5f512bc9f5c2657e30ef5
Kay MacDonald <kay.paz.macdonald@gmail.com>
2019-06-04 14:20:19 -0700

    first version of script finished

--- monotonesearch.py
+++ monotonesearch.py
@@ -7,36 +7,41 @@ import numpy as np
 
 examples = []  # this is where we'll store any violations we find
 
-for alphaSize in range(2,9): # alphabet size
+for alphaSize in range(2,6): # alphabet size
 
     for adjInt in range(0, (2**(alphaSize**2) - 1)): # matrices as wrapped binary integers
 
         # convert the integer for a matrix into the matrix itself, called adj
     
-        strList = list((bin(adjInt)))
-        tooShort = (alphaSize**2) - len(strList)
-        strList = (['0'] * tooShort) + strList
+        strList = list((bin(adjInt)[2:]))
+        intList = [int(i) for i in strList]
+        tooShort = (alphaSize**2) - len(intList)
+        intList = ([0] * tooShort) + intList
     
-        adj = np.array(strList)).reshape(alphaSize,alphaSize)
+        adj = np.array(intList).reshape(alphaSize,alphaSize)
 
-        # check if adj is essential and store the answer as a bool
+        # check if adj is essential and store the answer as a bool isEssential
+        myZeros = np.zeros(alphaSize, dtype=int)
 
-        isEssential = True
+        rowsGood = True
+        colsGood = True
 
-        
-    
- 
+        for i in range(0,alphaSize):
+            rowsGood = rowsGood and not(np.array_equal(adj[i,:], myZeros))
+            colsGood = colsGood and not(np.array_equal(adj[:,i], myZeros))
 
-# if adj is not essential, throw it away
+        isEssential = rowsGood and colsGood
+
+        # if adj is essential, check if it's an example of nonmonotonicity
+
+        if isEssential:
+
+            adjSquared = adj.dot(adj)
+
+            if np.sum(adjSquared) > (float(np.sum(adj)))**1.5:
+
+                examples.append(adj) 
 
 
-# else (i.e. if adj is essential), store its square as adjSquared
-
-
-# let b2 and b3 be the entry sums of adj and adjSquared respectively
-
-
-# if b3 > (float(b2))^(1.5) then append shiftNumber to examples
-
-
-# print each example as a new line
+for ex in examples:
+    print(ex)
